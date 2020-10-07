f6c4b0940ef6e54a8aefea67264c8b3e442bcf25
mohamedBAF <mohamed.benabedelfattah@esprit.tn>
2020-10-07 15:48:16 +0200

    Updates

---
+++ precache-manifest.dc2a24406b16a3153f299e52328bd322.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db58cf9f4444e849068e48ffe24c07a",
+    "url": "/facebookClone/index.html"
+  },
+  {
+    "revision": "9005d82ab89608425567",
+    "url": "/facebookClone/static/css/main.76d05703.chunk.css"
+  },
+  {
+    "revision": "a950d140ea52e97d27fe",
+    "url": "/facebookClone/static/js/2.698278cf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/facebookClone/static/js/2.698278cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9005d82ab89608425567",
+    "url": "/facebookClone/static/js/main.8277c6d2.chunk.js"
+  },
+  {
+    "revision": "b0a7c0ccebe6dd7f5447",
+    "url": "/facebookClone/static/js/runtime-main.39abe897.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/facebookClone/static/media/logo.5d5d9eef.svg"
+  }
+]);
